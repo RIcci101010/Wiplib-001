fb994d3d756aeaa44c50feefe815a9f9d4f5e951
andrew ricci <andrewxricci2011@gmail.com>
2026-08-31 11:44:44 -0400

    Impliments CANdi and CANrange, and simplifed code, can be called (inital commit 3)

--- src/ElevatorSubsystem.java
+++ src/ElevatorSubsystem.java
@@ -1,44 +1,35 @@
 package frc.robot;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANdi;
+import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
-import au.grapplerobotics.LaserCan;
+
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ElevatorSubsystem extends SubsystemBase {
-    private final TalonFX elevatorMotor = new TalonFX(1);
-    private final LaserCan distanceThing = new LaserCan(2);
-    private final DigitalInput bottomLimit = new DigitalInput(3);
+    private final TalonFX elevatorMotor = new TalonFX(5);
+    private final CANrange distanceThing = new CANrange(6);
+    private final CANdi bottomLimit = new CANdi(7);
     private StatusSignal<Angle> motorPos = elevatorMotor.getPosition();
-    private final double rotationsPerMM = 3;
-    private final double totalHeightMM = 200;
-    private final double scoringPosition = totalHeightMM * rotationsPerMM;
+    private final double rotationsPerInch = 0.5; // rotations per inch
+    private final double totalHeight = 30; // shooting heigh in inches
 
-    public Command zeroElevator() {
+public double getHeightInches() { // calcuates bots height in inches i hope
+  motorPos.refresh();
+  return motorPos.getValueAsDouble() / rotationsPerInch;
+}
+    public Command zeroElevator() { // zeros motors 
         return run(() -> elevatorMotor.set(-0.2))
-        .until(() -> bottomLimit.get())
+        .until(() -> bottomLimit.getS1Closed.getValue())
         .finallyDo(() -> {
             elevatorMotor.set(0.0); 
             motorPos.refresh(); 
             elevatorMotor.setPosition(0);
             });
     }
-    public boolean hasGamePiece() {
-        return distanceThing.getMeasurement().distance_mm < 75;         
-}
-public double scoringPos() {
-return scoringPosition;
-}
-public Command moveToScoreHeight() {
-    return zeroElevator().andThen(
-        run(() -> { motorPos.refresh(); elevatorMotor.set(0.2); })
-   .until(()-> {motorPos.refresh(); motorPos.getValueAsDouble() >= scoringPosition;})
-    .finallyDo(() -> {
-       elevatorMotor.stopMotor(); 
-        })
-    );
-}
+public Command MoveToScore() {
+return run(() -> elevatorMotor.set(0.2)).until(getHeightInches() >= totalHeight).finallyDo(() -> elevatorMotor.set(0.0));
 }
 
+}
