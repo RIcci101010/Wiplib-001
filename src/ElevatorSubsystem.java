330ab92b9d426a07e63d36b9b58bdcab3d3a2990
andrew ricci <andrewxricci2011@gmail.com>
2026-09-01 17:59:36 -0400

    Hopyfully got PID working

--- src/ElevatorSubsystem.java
+++ src/ElevatorSubsystem.java
@@ -3,8 +3,9 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANdi;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.oenix6.configs.TalonFXConfiguration;
-import com.ctre.oenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.PositionVoltage;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -25,6 +26,8 @@ public class ElevatorSubsystem extends SubsystemBase {
         configs.slot0.kF = 0.3;
         elevatorMotor.getConfigurator().apply(configs);
     }
+private final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+
 public double getHeightInches() { // calcuates bots height in inches i hope
   motorPos.refresh();
   return motorPos.getValueAsDouble() / rotationsPerInch;
@@ -39,9 +42,12 @@ public double getHeightInches() { // calcuates bots height in inches i hope
           });
 }
 public Command MoveToScore() {
-    return run(() -> elevatorMotor.set(0.2)) // want to put pid in for 0.2 in .set
-        .until(getHeightInches() >= totalHeight)
-        .finallyDo(() -> elevatorMotor.set(0.0));
+    return run(() -> elevatorMotor.setControl(
+      m_request.setControl(totalHeight * rotationsPerInch)));
+      
+    
+    // .until(getHeightInches() >= totalHeight)   i dont think i need these anymore, shouldnt pid just keep then at set point.
+       // .finallyDo(() -> elevatorMotor.set(0.0));
   }
 
 }
